cf4a11c3b21dc153a9730612b2fd48ba1ef70919
Mastik20 <illia.kharytskyi@viti.edu.ua>
2025-07-01 21:23:01 +0300

    Добавьте файлы проекта.

---
+++ EX_1/HostData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PacketDataAnalyzer
+{
+    public class HostData
+    {
+        public DateTime DateTime { get; set; }
+        public string Host { get; set; }
+        public int Packets { get; set; }
+        public int Size { get; set; }
+        public int TotalData => Packets * Size;
+    }
+}
